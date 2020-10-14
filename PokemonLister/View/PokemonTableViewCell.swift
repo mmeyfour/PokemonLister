74cb9c01acc878d0b7cb8be339cf4c6fdafd8a6d
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-14 11:30:11 +0200

    tebleView cell created image & text pokemon created

--- PokemonLister/View/PokemonTableViewCell.swift
+++ PokemonLister/View/PokemonTableViewCell.swift
@@ -9,15 +9,13 @@ import UIKit
 
 class PokemonTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonTextLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
